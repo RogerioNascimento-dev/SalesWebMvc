867a4f89e44cedcd1504dcc7d44e9748b67decc1
Rogéario Nascimento <rogerio.nascimento@soterotech.com.br>
2022-08-14 00:45:44 -0300

    Configuração e CRUD funcional de Departments

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using SalesWebMvc.Models.ViewModels;
+using SalesWebMvc.Models;
 namespace SalesWebMvc.Controllers
 {
     public class HomeController : Controller
